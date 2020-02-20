d74d6151f4c5bb13da703b536f9e99dc1ffd76eb
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-02-20 20:59:18 +0900

    commonパッケージにGlobalExceptionHandlerクラスを追加

---
+++ src/main/java/jp/co/sample/emp_management/common/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package jp.co.sample.emp_management.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * アプリケーション内で処理されなかった例外をここでキャッチし、
+ * エラーページへ遷移させます.
+ * 
+ * @author igamasayuki
+ *
+ */
+@Component
+public class GlobalExceptionHandler implements HandlerExceptionResolver {
+
+	private static final Logger LOGGER
+			= LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+	/* (non-Javadoc)
+	 * @see org.springframework.web.servlet.HandlerExceptionResolver#resolveException(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.lang.Object, java.lang.Exception)
+	 */
+	@Override
+	public ModelAndView resolveException(
+			HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object obj, 
+			Exception e) {
+		LOGGER.error("システムエラーが発生しました！", e);
+		return null; // ←500エラーが発生したら自動的にerror/500.htmlに遷移してくれる
+	}
+}
